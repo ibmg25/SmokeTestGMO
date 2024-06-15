12818b64943c0177464d3cf97206fe2e9b82d600
Guillermo <guichi.maldo@gmail.com>
2024-06-15 02:46:53 -0400

    se implemetno la mejora de realizar compra

--- features/RealizarCompra.feature
+++ features/RealizarCompra.feature
@@ -6,3 +6,25 @@ Feature: Buy Products
   Background:
    Given I am on the GMO 
    When I click the button Enter GMO OnLine button
+  
+  Scenario: Place a Empty order
+  
+  When Click on the Place and orden Button
+  Then I should see an alert "Please Order Something First"
+
+  Scenario Outline: Select Multiple Products
+
+  When I update the quantities of the following products:
+     | Product Name           | Quantity |
+    | 3 Person Dome Tent    | 2        |
+    | External Frame Backpack| 1        |
+    | Glacier Sun Glasses    | 3        |
+    | Padded Socks           | 4        |
+    | Hiking Boots           | 1        |
+    | Back Country Shorts    | 2        |
+   
+  And Click on the Place and orden Button
+Then I should see the correct "<Tax>" the correct "<Shipping>" and the correct "<Subtotal>" and the total amount the sum:
+Examples:
+|Subtotal | Shipping | Tax | 
+|$ 1223.66 |$ 5.00 | $ 61.18| 
--- features/step_definitions/RealizarCompra.rb
+++ features/step_definitions/RealizarCompra.rb
@@ -18,40 +18,64 @@ end
 When('Click on the Reset form Button') do  
     find('body > form > table > tbody > tr:nth-child(3) > td > div > center > table > tbody > tr > td > p > input[type=reset]:nth-child(1)').click
 end
-
+$global_variable=0
 When('I update the quantities of the following products:') do |table|
+    $global_variable=0
     table.hashes.each do |row|
         product_name = row['Product Name']
         quantity = row['Quantity']
         update_product_quantity(product_name, quantity)
+        check_product_quantity_to_get_total(product_name , quantity)
+        
     end
 end
 
-Then('the TotalAmount should be {string}') do |expected_total|
-    # Encuentra la última fila de la tabla
+    
+Then('I should see the correct {string} the correct {string} and the correct {string} and the total amount the sum:') do |expected_tax, expected_shipping, expected_subtotal|
+
+    expected_total=$global_variable+($global_variable*0.05)+5
+
+    expected_total = expected_total.round(2)
+
+    product_total=find('body > form > table > tbody > tr:nth-child(1) > td > div > center > table > tbody > tr:nth-last-child(4) > td:nth-child(3)').text
+    actual_tax =find('body > form > table > tbody > tr:nth-child(1) > td > div > center > table > tbody > tr:nth-last-child(3) > td:nth-child(2)').text
+    actual_shipping = find('body > form > table > tbody > tr:nth-child(1) > td > div > center > table > tbody > tr:nth-last-child(2) > td:nth-child(2)').text
     actual_total = find('body > form > table > tbody > tr:nth-child(1) > td > div > center > table > tbody > tr:last-child > td:nth-child(2)').text
-        # Compara el valor total actual con el esperado
+
+    numeric_string = actual_total.gsub(/[^\d.]/, '')
+    actual_total = numeric_string.to_f
     if actual_total == expected_total
-        puts "El valor del total es correcto: #{actual_total}"
+        puts "El valor total  del los productos es correcto: #{actual_total}"
     else
         puts "El valor del total es incorrecto. Se esperaba #{expected_total} pero se encontró #{actual_total}"
     end
-end
-Then('quantity of all the products must be :') do |table|
-    table.hashes.each do |row|
-        product_name = row['Product Name']
-        quantity = row['Quantity']
-        check_product_quantity(product_name, quantity)
+    if actual_shipping == expected_shipping
+        puts "El valor del envío es correcto: #{actual_shipping}"
+    else
+        puts "El valor del envío es incorrecto. Se esperaba #{expected_shipping} pero se encontró #{actual_shipping}"
+    end
+
+    if actual_tax == expected_tax
+        puts "El valor del impuesto es correcto: #{actual_tax}"
+    else
+        puts "El valor del impuesto es incorrecto. Se esperaba #{expected_tax} pero se encontró #{actual_tax}"
+    end
+
+    if product_total == expected_subtotal
+        puts "El valor del subtotal es correcto: #{product_total}"
+    else
+        puts "El valor del total es incorrecto. Se esperaba #{expected_subtotal} pero se encontró #{product_total}"
     end
 end
+
 Then('I should see an alert {string}') do |expected_alert_text|
-    # Espera a que la alerta aparezca
+
     page.driver.browser.switch_to.alert.text == expected_alert_text
     alert = page.driver.browser.switch_to.alert
     actual_alert_text = alert.text
     if actual_alert_text == expected_alert_text
         puts "El mensaje de la alerta es correcto: #{actual_alert_text}"
-        alert.accept # O alert.dismiss si deseas rechazar la alerta
+        alert.accept 
     else
         raise "El mensaje de la alerta es incorrecto. Se esperaba '#{expected_alert_text}' pero se encontró '#{actual_alert_text}'"
     end
@@ -78,6 +102,21 @@ def check_product_quantity(product_name, quantity)
             if row.visible?
                 last_column_cell = row.find('td:last-child input[type="text"]')
                 expect(last_column_cell.value).to eq(quantity)
+                break
+            end
+            counter += 1
+        end
+end
+def check_product_quantity_to_get_total(product_name,quantity)
+    counter = 2
+        while counter < 7
+            row_selector = "body > form > table > tbody > tr:nth-child(#{counter}) > td > div > center > table > tbody > tr"
+            row = find(:css, row_selector, text: product_name)
+            if row.visible?
+                total=row.find('td:nth-child(3)').text
+                numeric_string = total.gsub(/[^\d.]/, '')
+                total_float = numeric_string.to_f
+                $global_variable=($global_variable) + (total_float*quantity.to_i)
                 break
             end
             counter += 1
